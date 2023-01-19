9bda95abccef4e7bf8f0c7d7d7e774ababb19f2d
luismateosarmada <99286771+luismateosarmada@users.noreply.github.com>
2023-01-19 16:09:02 +0100

    Add files via upload

---
+++ zSUEÑOS para la nueva sociedad videojuegos y música.php
@@ -0,0 +1,48 @@
+ANTES DE LAS 2 IMÁGENES (MÚSICA Y VIDEOJUEGOS), ¡A TRAGARSE TEXTO JE JE! <BR>
+<BR>Para los próximos días, ¿sueño la sociedad más civilizada (tiene que ser equilibrada, y dormir mucho, pues quien tiene salud / juventud, duerme)? ¿Se puede debatir en una cafetería a la que vaya gente a la que la imagen le importe un bledo?:
+<BR>- ¡ABAJO EL PIJISMO / SIBARITISMO EN LA LIMPIEZA! Casi no habrá limpiadores obsesivos, es más, la gente, por lo general, no usará jabón nada más que para las manos y las axilas. Las duchas son para abrir poros, y ducharse muy a menudo fastidia la piel.
+<BR>* NO hará falta agua caliente, puesto que los pies se los puede uno lavar a diario o, ¡pasar las manos 2 veces al día y lavárselas (teniendo cuidao de no comer nada justo a continuación, ya se encarga la naturaleza de que todo vuelva al equilibrio con unos pocos minutos)!
+<BR>* La ropa, si la aireas (meneándola...), pasa a oler mejor (si llevas tiempo sin ponerla...).
+<BR>* ¡Hasta los de pelo graso pueden hacer lo de la manos! ¿Y dónde se limpian las manos de grasa? NO sé, de rodillas p'abajo (el cuerpo se recupera).
+<BR>- Ahora tenemos una psicopatocracia (2022). La meritocracia está mal, pues el meritorio, tiene que saber algo de moralidad / ética. Por lo tanto, lo ideal, es una sabiocracia (sabio de la montaña...).
+<BR>* ¿Y cómo consultamos en caso de buscar soluciones técnicas? ¡Por llamada "desde arriba", y hala, al bien común, que las empresas de hoy sólo entienden de ganancias, y el amor y las ganas de simple poder, son contrarios se puede decir!
+<BR>- Corresponde a las familias el educar a los hijos, y a los maestros, mejorar lo de las familias, y además, enseñar alguna actividad...
+<BR>* Los vecinos y familiares cuidarán de los niños, no unos locos adoctrinados, que en cuanto oyen "eres una víctima por tal...", se quedan prendados.
+<BR>* NO olvidarse de 2 cosas del Budismo: el sufrimiento lo causa el deseo vehemente (que causa apego, a que lo agradable continúe, a que lo desagradable pare...). Cuidado con cómo manejamos la frustración, que nos lleva al deseo. Los errores hay que aceptarlos, y puede que tengamos que encontrar nuestras debilidades psicológicas, para corregirlos.
+<BR>* Por último, toda la información será gratis y accesible. ¡Los secretos hacen daño, generan enfermedades! El que tenga el mejor "batido de chocolate", expandirá la receta, ¡basta ya de materialismo o narcisismo!
+<BR>- Las apariencias engañan:
+<BR>* Los trabajadores maniáticos, suelen tener síndrome de asperger, así que, en cuanto se curen, trabajaran menos (trabajar mucho no es para la gente sana), se les irá el "poder tecnológico"... Los trabajos oficiales podrían desaparecer, porque estar "colocado" va en contra de hacer cosas útiles para gente elevada.
+<BR>* La simpatía es propia del psicópata, así que no estés orgulloso de ser simpático, y profundiza, verás lo que ocultas ahí dentro para dar una imagen falsa.
+<BR>- Más:
+<BR>* Todo está ya inventao (lo ocultan las élites, que ocultan la Histroia...) y creao (tenemos archivos .mp3 de sobra para curarnos). La gente no hará nada por sentirse útil, sino sólo cosas útiles / moralmente perfectas, para gente elevada (o lo intentarán).
+<BR>* Todos somos diferentes (misión, mirar padentro o más pa fuera...). Se respetarán la Lunas (la Llena es de trabajar físicamente más, por lo general...).
+<BR>* La gente disfrutará de lo que da la naturaleza (la brisa...).
+<BR>* Sin miedo, nos veremos grandes creadores. Y veo a gente dándose cuenta de que si deja "A", vendrá la siguiente fase de su camino. Y sabes que mejorando la psique profundamente (haciendo camino), tienes más éxito, atraes a gente mejor, aunque no tremendamente exitosa, que por lo general son enfermos mentales que no han necesitado transformarse... ¡Escraches a los de "la vida cómoda" (almas poco avanzadas por lo general, que han nacido con ciertas capacidades, y se han despreocupao de si hacen o no el mal, tratando a los demás como si tuvieran estas capacidades, que en muchos casos son terribles: intelectualidad, cursilería, incapacidad de estarse quietos horas y horas...)! Y se deja "A" manejando bien la incertidumbre, porque se sabe que la "vida" genera "casualidades", sobre todo si estás tranquilo, eres positivo, y deseas para ti (en plan "no tengo esto porque me falta esto en la psique"), y sueñas para la sociedad. OPERAR DIRECTAMENTE EN EL MUNDO MATERIAL, ES DE GENTE MUY POCO SUTIL.
+<BR>* La gente irá a su psique sin echar culpas afuera.
+<BR>
+<BR>ANTES DE IR DEFINITIVAMENTE CON LA MÚSICA Y LOS VIDEOJUEGOS, 3 "DE VER": VAYA SEMANITA (EITB, SE METEN CON TODO CON LIBERTAD), Y 2 PELÍCULAS SELECCIONADAS POR GOERREIG POR SUS VALORES MORALES (LOS MALOTES DE HOLLYWOOD NO SE HAN ATREVIDO A HACER REMAKES "MODERNOS" DE ELLAS): LA HISTORIA INTERMINABLE (SE TRATA DE DESPERTAR LA PARTE FEMENINA, INICIANDO "EL FUEGO" CON LA MASCULINA), Y CONAN I (NO EL II), DONDE CONAN VA DE LO MATERIAL (FEMINEIDAD DE BAJO NIVEL) A LO SUTIL (FEMINEIDAD DE ALTO NIVEL).
+<BR>
+<img src="imag2.jpg">
+<BR>- Perales, un grandísimo autor, con algunas producciones increíbles (30 Grandes Canciones). Descúbrelo, sin el miedo del machista.
+<BR>- Talk Talk, los inventores del postrock (y no Radiohead). Fueron cambiando de algo "technopop" a algo grandioso. Hay que escucharse el disco de principio a fin para ver lo grandes que fueron.
+<BR>- Génesis y Phil Collins, nos abren un mundo musical maravilloso, que incluye canciones "para gritar como Phil", y desahogarse.
+<BR>- Si quieres tener clase musical...
+<BR><img src="imag3.jpg">
+<BR>- Outrun (arcade, SEGA, Japón, 1986): anticipación, reflejos medios, poca necesidad de memoria... y fácil de terminar. Jugar Drive Out.
+<BR>- Golden Axe (arcade, SEGA, Japón, 1989): jugable y bello hasta la médula (el jefe del final puede frustrarte, eso sí). Ver DJ Boy.
+<BR>- Pang (Amiga, OCEAN, original arcade Mitchell / Capcom, Japón, 1989): un juego mitad puzzle, mitad de esquivar y plataformear.
+<BR>- Lemmings (Amiga, DMA Design / Psygnosis, UK, 1991): puzzle de construcción. Hay que buscar soluciones, con recursos limitados.
+<BR>- Black Tiger (Arcade, Capcom, Japón, 1987): quizás el "arcade bárbaro" + jugable y bello (Istroia). Ver "Ghouls", Shinobi y Tiger Road.
+<BR>- Shadow of the Beast III (Amiga, Reflections / Psygnosis, UK, 1992): belleza, arcade + puzzle... lo tiene todo. Ver el II (FMTowns).
+<BR>- Leaderboard (Amiga, US Gold, UK, 1987 / 1988): colores suaves, se entiende en poco tiempo cómo funciona, relaja... ¿Para qué más?
+<BR>- Tickland (Spectrum, Juan Antonio Paz Salgado, España): el personaje va de cuadrícula en cuadrícula, lo cual hace el control + fácil.
+<BR>- Manic Miner (Spectrum, Software Projects, UK, 1983): ¡el rey de las videoaventuras de una sola pantalla! Superaba a los arcades.
+<BR>- Head over Heels (Spectrum, Ocean, UK, 1987): bella videoaventura, juntando y separando caminos de 2, en vez de complicarla (objetos).
+<BR>- Prince of Persia (PC, Jordan Mechner / Ubisoft, USA / Francia): esos movimientos, ese desarrollo cinematográfico... Una joya.
+<BR>- Buscaminas Windows (PC, Microsoft, USA, 1989): lógica, concentración, no dar nada por sentado... Infinitamente jugable.
+<BR>- Bruce Lee (Spectrum, Datasoft / OCEAN, 1984): es una aventura tan sugerente y a la vez jugable (y graciosa)... ¡Menudos decorados!
+<BR>- Profanation (Abu Simbel; Spectrum, Dinamic, España, 1985): aventura de ajuste fino, con decorados impresionantes. Jugar Phantomas II.
+<BR>- TMNT Back from the Sewers (Game Boy original, Konami, Japón, 1991): un arcade explosivo, con música muy buena. Jugar Marioland II.
+<BR>- Tetris (Arcade, Alekséi Pázhitnov / Atari, 1984 / 1988, Rusia / USA): el juego de puzzle por excelencia. Jugar Boxy Boy.
+<BR>##Menciones: Turrican III, Astro Marine Corps (Amiga, Spectrum 128 kb).
+<BR><img src="imag4.jpg">
