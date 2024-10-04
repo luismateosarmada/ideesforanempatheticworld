e50a9ea9201d2d3b228694a2eddba941e616ad03
luismateosarmada <99286771+luismateosarmada@users.noreply.github.com>
2024-10-04 20:52:35 +0200

    Add files via upload

---
+++ enciclopedia.php
@@ -0,0 +1,160 @@
+<?php
+ if (!isset ($_GET["estado"])) 
+ {
+  Menu_Principal();
+ }
+  else
+  {
+   $tema = $_GET["estado"];
+   if ($tema == "principal") Menu_Principal();
+   if ($tema == "importante") Tema_importante();
+   if ($tema == "psicosalud") Tema_psicosalud();
+   if ($tema == "sociosuenos") Tema_sociosuenos();
+   if ($tema == "histoactualidad") Tema_histoactualidad();
+   if ($tema == "tecnicas") Tema_sociosuenos();
+   if ($tema == "anexos") Tema_anexos();
+  }
+?> 
+
+<?php
+function Menu_Principal()
+{
+ global $PHP_SELF;
+ include 'principal.txt';
+?>
+<FORM ACTION="<?php echo $PHP_SELF; ?>" METHOD=GET>
+<INPUT TYPE=hidden NAME="estado" VALUE="importante">
+<INPUT TYPE=submit VALUE="LO IMPORTANTE: misión vital y modelos de bondad y recreo / diversión">
+</FORM>
+<FORM ACTION="<?php echo $PHP_SELF; ?>" METHOD=GET>
+<INPUT TYPE=hidden NAME="estado" VALUE="psicosalud">
+<INPUT TYPE=submit VALUE="Psicología para tener salud">
+</FORM>
+<FORM ACTION="<?php echo $PHP_SELF; ?>" METHOD=GET>
+<INPUT TYPE=hidden NAME="estado" VALUE="sociosuenos">
+<INPUT TYPE=submit VALUE="Sociología y sueños">
+</FORM>
+<FORM ACTION="<?php echo $PHP_SELF; ?>" METHOD=GET>
+<INPUT TYPE=hidden NAME="estado" VALUE="histoactualidad">
+<INPUT TYPE=submit VALUE="Historia y actualidad">
+</FORM>
+<FORM ACTION="<?php echo $PHP_SELF; ?>" METHOD=GET>
+<INPUT TYPE=hidden NAME="estado" VALUE="tecnicas">
+<INPUT TYPE=submit VALUE="Técnicas">
+</FORM>
+<FORM ACTION="<?php echo $PHP_SELF; ?>" METHOD=GET>
+<INPUT TYPE=hidden NAME="estado" VALUE="anexos">
+<INPUT TYPE=submit VALUE="Anexos">
+</FORM>
+<?php
+}
+?>
+
+<?php
+function Tema_importante()
+{
+ global $PHP_SELF;
+?>
+<FORM ACTION="<?php echo $PHP_SELF; ?>" METHOD=GET>
+<?php
+ include 'importante.txt';
+?>
+<INPUT TYPE=hidden NAME="estado" VALUE="principal">
+<INPUT TYPE=submit VALUE="Volver">
+</FORM>
+<?php
+}
+?>
+
+<?php
+function Tema_psicosalud()
+{
+ global $PHP_SELF;
+?>
+<FORM ACTION="<?php echo $PHP_SELF; ?>" METHOD=GET>
+<?php
+ include 'psicosalud.txt';
+?>
+<INPUT TYPE=hidden NAME="estado" VALUE="principal">
+<INPUT TYPE=submit VALUE="Volver">
+</FORM>
+<?php
+}
+?>
+
+<?php
+function Tema_sociosuenos()
+{
+ global $PHP_SELF;
+?>
+<FORM ACTION="<?php echo $PHP_SELF; ?>" METHOD=GET>
+<?php
+ include 'sociosuenos.txt';
+?>
+<INPUT TYPE=hidden NAME="estado" VALUE="principal">
+<INPUT TYPE=submit VALUE="Volver">
+</FORM>
+<?php
+}
+?>
+
+<?php
+function Tema_histoactualidad()
+{
+ global $PHP_SELF;
+?>
+<FORM ACTION="<?php echo $PHP_SELF; ?>" METHOD=GET>
+<?php
+ include 'histoactualidad.txt';
+?>
+<INPUT TYPE=hidden NAME="estado" VALUE="principal">
+<INPUT TYPE=submit VALUE="Volver">
+</FORM>
+<?php
+}
+?>
+
+<?php
+function Tema_tecnicas()
+{
+ global $PHP_SELF;
+?>
+<FORM ACTION="<?php echo $PHP_SELF; ?>" METHOD=GET>
+<?php
+ include 'tecnicas.txt';
+?>
+<INPUT TYPE=hidden NAME="estado" VALUE="principal">
+<INPUT TYPE=submit VALUE="Volver">
+</FORM>
+<?php
+}
+?>
+
+<?php
+function Tema_anexos()
+{
+ global $PHP_SELF;
+?>
+<FORM ACTION="<?php echo $PHP_SELF; ?>" METHOD=GET>
+<?php
+ include 'anexos.txt';
+?>
+<INPUT TYPE=hidden NAME="estado" VALUE="principal">
+<INPUT TYPE=submit VALUE="Volver">
+</FORM>
+<?php
+}
+?>
+
+principal.txt:
+
+DISFRUTAR MÁS SIENDO RESPONSABLE Y MODESTO: MISIÓN VITAL Y LIMPIEZA DE PSIQUE.
+</BR>Responsable quiere decir equilibrado, entre otras cosas. Gracias a Cristian (Zeballos), Marco de Torrelavega: La Última ReEvolución...
+</BR>
+</BR>Breve introducción:
+</BR>- ¿Raro esto que digo de ser responsable para ser más feliz NO? Para ser responsable, yo diría que hay que saber la misión espiritual de vida (se calcula el llamado Nodo Norte...) y seguirla. Cuando tu actividad está en consonancia con la misión (parecida a "lo que te mueve"), la vida es más suave (estás en tu lugar). La vida se endureció con el trabajo asalariado ("invento masón"): nos llevaron (por estupidez u obligación) a las ciudades, y se favoreció a empresas explotadoras. Ahora la gente trabaja para que las estadísticas sean buenas, para que la gente no se rebele (así que las élites quieren que estés ocupado en "nada" / "humo").
+</BR>- ¿Qué significa lo de modesto? Que se busquen, NO logros grandes y deslumbrantes, sino propósitos para el crecimiento personal (que define tu bondad; al crecer / superar algo que "desconcierta a tu psique", desreprimes creencias o simplemente las cambias, y eso hace que reacciones mejor, puesto que las represiones te dominan inconscientemente). Y así, terminas más fuerte, y feliz con poco. Todo es Psicología: enfermedades...
+</BR>* Si los logros NO te contentan, puedes NO estar conectado con un propósito claro / determinado: los obstáculos pierden sentido (en vez de tener un significado, apreciando el camino, que es lo importante, ser bueno). El propósito motiva (es un faro), y buscas escenarios sociales con gusto (a pesar de las reacciones dolorosas) para tener experiencias que te acerquen a la solución psicológica... en vez de huir. Establece submetas a corto y largo plazo; ajusta tu propósito con el tiempo; y agradece (eso te "anclará" al proceso).
+</BR>* Cuando desreprimes / te vuelves bondadoso (fuerte), la cara es más serena, menos enfermizamente risueña o "activa". Ojo, los psicópatas llevan máscara.
+</BR>
+</BR>
