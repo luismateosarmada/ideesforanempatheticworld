91992d3d0a5f2f14980a164ffc37649f22535b53
luismateosarmada <99286771+luismateosarmada@users.noreply.github.com>
2025-06-03 16:09:24 +0200

    Update 00000000000000000000000000000000000todomisiones.php

--- 00000000000000000000000000000000000todomisiones.php
+++ 00000000000000000000000000000000000todomisiones.php
@@ -1,95 +1,223 @@
 <?php
  //En construcción. Los .PHPs se pueden ejecutar con el Xampp... Este .PHP, es para mí, sobre todo. Quiero saber datos de la gente para ayudarme a encontrar su vocación y animarlos a volar. Es un.PHP muy sencillo (NO hay Javascript...), aunque muy largo, y lleva semanas hacerlo (porque sólo voy a dedicar un pequeño porcentaje de mi tiempo). Paciencia y confianza.
 
- // Carta de revolución solar (entre cumpleaños).
- // Júpiter Géminis 12. ¿Expando el conocimiento espiritual (12)? 
- // Saturno Piscis. ¿Diluyo las ilusiones?
- // Urano Tauro 11. ¿Proyectos sociales que duran y se consolidan en grupos (11)?
- // Neptuno Piscis 10. ¿Mucha espiritualidad en el trabajo?
- // Casa 11 Aries. ¿Ser pionero en grupos / estructuras sociales?
- // MIS DATOS:
- //$dia = 3;
- //$anio = 1977;
- //$casa2 = 11;
- //$casa10 = 8;
- //$ascendente = 10;
- //$luna = 5;
- //$mes = 11;
- //$cartaanualmision = 4;
- //$cartaanualhabilidad = 8;
- //$mutable = 1;
- //$fijo = 0;
- //$cardinal = 0;
- //$pasivo = 1;
- //$dependiente = 1;
- //$privado = 1;
- //$anualcasa2 = 5;
- //$anualcasa10 = 12;
- //$nombre = "Luis Mateos Armada";
- //$caracmanifestante = 0;
- //$generador = 1;
- //$proyector = 0;
- //$reflector = 0;
- //$manifestante = 0;
- //$mision = 46;
- //$chino = 6;
- //$hombre = 1;
- //$manocuadrada = 0;
- //$manoespatula = 1;
- //$manogigante = 0;
- //$manoconica = 0;
- //$manonudosa = 1;
- //$manoespiritual = 0;
- $dia = 3;
- $anio = 1977;
- $casa2 =11;
- $casa10 = 8;
- $ascendente = 10;
- $luna = 5;
- $mes = 11;
- $cartaanualmision = 4;
- $cartaanualhabilidad = 8;
- $mutable = 1;
- $fijo = 0;
- $cardinal = 0;
- //Poner 2 para anular.
- $pasivo = 1;
- $dependiente = 1;
- $privado = 1;
- $anualcasa2 = 5;
- $anualcasa10 = 12;
- $signo = calcularsigno($dia, $mes);
- astrologia();
- $nombre = "Luis Mateos Armada";
- $numeneagrama = calcularnumeneagrama($dia, $mes, $anio);
- eneagrama();
- aniosdecambio();
- nombreroscopo();
- $num = calcularnumnodonorte(); 
- nodonorte($num);
- tzolkinmaya();
- $caracmanifestante = 0;
- $generador = 1;
- $proyector = 0;
- $reflector = 0;
- $manifestante = 0;
- $mision = 46;
- $chino = 6;
- diseniohumano();  
- cejas();
- $hombre = 1;
- $numerokwa = calcularnumerokwa();
- kwa();
- zodchino();
- personaljung();
- $manocuadrada = 0;
- $manoespatula = 0.25;
- $manogigante = 0;
- $manoconica = 0;
- $manonudosa = 1;
- $manoespiritual = 0;
- manos();
- diseniomiscanales();
+ if (!isset ($_GET["cajaanio"])) datos();  
+ else
+ {
+  // Carta de revolución solar (entre cumpleaños).
+  // Júpiter Géminis 12. ¿Expando el conocimiento espiritual (12)? 
+  // Saturno Piscis. ¿Diluyo las ilusiones?
+  // Urano Tauro 11. ¿Proyectos sociales que duran y se consolidan en grupos (11)?
+  // Neptuno Piscis 10. ¿Mucha espiritualidad en el trabajo?
+  // Casa 11 Aries. ¿Ser pionero en grupos / estructuras sociales?
+  // MIS DATOS:
+  //$dia = 3;
+  //$anio = 1977;
+  //$casa2 = 11;
+  //$casa10 = 8;
+  //$ascendente = 10;
+  //$luna = 5;
+  //$mes = 11;
+  //$cartaanualmision = 4;
+  //$cartaanualhabilidad = 8;
+  //$mutable = 1;
+  //$fijo = 0;
+  //$cardinal = 0;
+  //$pasivo = 1;
+  //$dependiente = 1;
+  //$privado = 1;
+  //$anualcasa2 = 5;
+  //$anualcasa10 = 12;
+  //$nombre = "Luis Mateos Armada";
+  //$caracmanifestante = 0;
+  //$generador = 1;
+  //$proyector = 0;
+  //$reflector = 0;
+  //$manifestante = 0;
+  //$mision = 46;
+  //$chino = 6;
+  //$hombre = 1;
+  //$manocuadrada = 0;
+  //$manoespatula = 1;
+  //$manogigante = 0;
+  //$manoconica = 0;
+  //$manonudosa = 1;
+  //$manoespiritual = 0;
+  $dia = 3;
+  $anio = $_GET["cajaanio"];
+  $casa2 =11;
+  $casa10 = 8;
+  $ascendente = 10;
+  $luna = 5;
+  $mes = 11;
+  $cartaanualmision = 4;
+  $cartaanualhabilidad = 8;
+  $mutable = 1;
+  $fijo = 0;
+  $cardinal = 0;
+  //Poner 2 para anular.
+  $pasivo = 1;
+  $dependiente = 1;
+  $privado = 1;
+  $anualcasa2 = 5;
+  $anualcasa10 = 12;
+  $signo = calcularsigno($dia, $mes);
+  astrologia();
+  $nombre = "Luis Mateos Armada";
+  $numeneagrama = calcularnumeneagrama($dia, $mes, $anio);
+  eneagrama();
+  aniosdecambio();
+  nombreroscopo();
+  $num = calcularnumnodonorte(); 
+  nodonorte($num);
+  tzolkinmaya();
+  $caracmanifestante = 0;
+  $generador = 1;
+  $proyector = 0;
+  $reflector = 0;
+  $manifestante = 0;
+  $mision = 46;
+  $chino = 6;
+  diseniohumano();  
+  cejas();
+  $hombre = 1;
+  $numerokwa = calcularnumerokwa();
+  kwa();
+  zodchino();
+  personaljung();
+  $manocuadrada = 0;
+  $manoespatula = 0.25;
+  $manogigante = 0;
+  $manoconica = 0;
+  $manonudosa = 1;
+  $manoespiritual = 0;
+  manos();
+  diseniomiscanales();
+ }
+
+ function datos()
+ {
+  global $PHP_SELF;
+  ?>
+  <FORM ACTION="<?php echo $PHP_SELF; ?>" METHOD=GET>
+  <select NAME="cajaanio">
+  <option VALUE="-1">--</option>
+  <option VALUE="1922">1922</option>
+  <option VALUE="1923">1923</option>
+  <option VALUE="1924">1924</option>
+  <option VALUE="1925">1925</option>
+  <option VALUE="1926">1926</option>
+  <option VALUE="1927">1927</option>
+  <option VALUE="1928">1928</option>
+  <option VALUE="1929">1929</option>
+ 
+  <option VALUE="1930">1930</option>
+  <option VALUE="1931">1931</option>
+  <option VALUE="1932">1932</option>
+  <option VALUE="1933">1933</option>
+  <option VALUE="1934">1934</option>
+  <option VALUE="1935">1935</option>
+  <option VALUE="1936">1936</option>
+  <option VALUE="1937">1937</option>
+  <option VALUE="1938">1938</option>
+  <option VALUE="1939">1939</option>
+ 
+  <option VALUE="1940">1940</option>
+  <option VALUE="1941">1941</option>
+  <option VALUE="1942">1942</option>
+  <option VALUE="1943">1943</option>
+  <option VALUE="1944">1944</option>
+  <option VALUE="1945">1945</option>
+  <option VALUE="1946">1946</option>
+  <option VALUE="1947">1947</option>
+  <option VALUE="1948">1948</option>
+  <option VALUE="1949">1949</option>
+
+  <option VALUE="1950">1950</option>
+  <option VALUE="1951">1951</option>
+  <option VALUE="1952">1952</option>
+  <option VALUE="1953">1953</option>
+  <option VALUE="1954">1954</option>
+  <option VALUE="1955">1955</option>
+  <option VALUE="1956">1956</option>
+  <option VALUE="1957">1957</option>
+  <option VALUE="1958">1958</option>
+  <option VALUE="1959">1959</option>
+
+  <option VALUE="1960">1960</option>
+  <option VALUE="1961">1961</option>
+  <option VALUE="1962">1962</option>
+  <option VALUE="1963">1963</option>
+  <option VALUE="1964">1964</option>
+  <option VALUE="1965">1965</option>
+  <option VALUE="1966">1966</option>
+  <option VALUE="1967">1967</option>
+  <option VALUE="1968">1968</option>
+  <option VALUE="1969">1969</option>
+
+  <option VALUE="1970">1970</option>
+  <option VALUE="1971">1971</option>
+  <option VALUE="1972">1972</option>
+  <option VALUE="1973">1973</option>
+  <option VALUE="1974">1974</option>
+  <option VALUE="1975">1975</option>
+  <option VALUE="1976">1976</option>
+  <option VALUE="1977">1977</option>
+  <option VALUE="1978">1978</option>
+  <option VALUE="1979">1979</option>
+
+  <option VALUE="1980">1980</option>
+  <option VALUE="1981">1981</option>
+  <option VALUE="1982">1982</option>
+  <option VALUE="1983">1983</option>
+  <option VALUE="1984">1984</option>
+  <option VALUE="1985">1985</option>
+  <option VALUE="1986">1986</option>
+  <option VALUE="1987">1987</option>
+  <option VALUE="1988">1988</option>
+  <option VALUE="1989">1989</option>
+
+  <option VALUE="1990">1990</option>
+  <option VALUE="1991">1991</option>
+  <option VALUE="1992">1992</option>
+  <option VALUE="1993">1993</option>
+  <option VALUE="1994">1994</option>
+  <option VALUE="1995">1995</option>
+  <option VALUE="1996">1996</option>
+  <option VALUE="1997">1997</option>
+  <option VALUE="1998">1998</option>
+  <option VALUE="1999">1999</option>
+
+  <option VALUE="2000">2000</option>
+  <option VALUE="2001">2001</option>
+  <option VALUE="2002">2002</option>
+  <option VALUE="2003">2003</option>
+  <option VALUE="2004">2004</option>
+  <option VALUE="2005">2005</option>
+  <option VALUE="2006">2006</option>
+  <option VALUE="2007">2007</option>
+  <option VALUE="2008">2008</option>
+  <option VALUE="2009">2009</option>
+
+  <option VALUE="2010">2010</option>
+  <option VALUE="2011">2011</option>
+  <option VALUE="2012">2012</option>
+  <option VALUE="2013">2013</option>
+  <option VALUE="2014">2014</option>
+  <option VALUE="2015">2015</option>
+  <option VALUE="2016">2016</option>
+  <option VALUE="2017">2017</option>
+  <option VALUE="2018">2018</option>
+  <option VALUE="2019">2019</option>
+
+  <option VALUE="2020">2020</option>
+  </select>
+
+  <INPUT TYPE=submit VALUE="OK">
+
+  </FORM>
+ <?php
+ }
 
  function astrologia()
  {
@@ -1276,7 +1404,7 @@ Exceso planetas: dependencia de la fortuna de los demás y estar atado a interes
   $cuenta = $cuenta + sumacifras($anio);
 
   // REDUCIENDO HASTA 11, 22, 33 O UNA CIFRA.
-  while ( ($cuenta > 10) && ($cuenta != 11) && ($cuenta != 22) && ($cuenta != 33) )
+  while ( ($cuenta >= 10) && ($cuenta != 11) && ($cuenta != 22) && ($cuenta != 33) )
    $cuenta = sumacifras($cuenta);
   return $cuenta;
  } 
